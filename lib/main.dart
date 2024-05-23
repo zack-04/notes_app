856b6b66b4be9fbbd8b75cd1b6a045140d89ec64
Krishna <bhatiakrishna529@gmail.com>
2024-05-23 17:55:33 +0530

    configured firebase into the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/firebase_options.dart';
 import 'package:notes_app/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,4 +26,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
